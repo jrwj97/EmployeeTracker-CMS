900e2bfcafe2dcaca72aaced79bb7c20b5960826
jrwj97 <jrwj97@aol.com>
2022-03-23 21:44:33 -0400

    added inital inquirer prompt and first few functions

--- db/schema.sql
+++ db/schema.sql
@@ -12,6 +12,8 @@ CREATE TABLE role (
   title: VARCHAR(30),
   salary: DECIMAL,
   department_id: INT
+  NDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 )
 
 CREATE TABLE employee (
@@ -19,5 +21,8 @@ CREATE TABLE employee (
   first_name: VARCHAR(30),
   last_name: VARCHAR(30),
   role_id: INT,
-  manager_id: INT
+  manager_id: INT,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  INDEX manager_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 )
